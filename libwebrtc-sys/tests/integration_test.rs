84cc04bb3ff94d78f805be04440ce2f515075bf1
logist322 <hytornoy-99@mail.ru>
2021-11-08 13:20:48 +0300

    Rework test

--- libwebrtc-sys/tests/integration_test.rs
+++ libwebrtc-sys/tests/integration_test.rs
@@ -1,6 +1,12 @@
+use std::thread::sleep_ms;
+
 use libwebrtc_sys::system_time_millis;
 
 #[test]
 fn it_works() {
-    system_time_millis();
+    let a: i32 = system_time_millis().parse().unwrap();
+    sleep_ms(2000);
+    let b: i32 = system_time_millis().parse().unwrap();
+
+    assert!((a - b) < 3000);
 }
