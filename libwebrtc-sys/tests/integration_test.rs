6470c3840cb143f8d7ad55bd6f3442cf9fe4015e
logist322 <hytornoy-99@mail.ru>
2021-11-08 13:24:17 +0300

    Fix test

--- libwebrtc-sys/tests/integration_test.rs
+++ libwebrtc-sys/tests/integration_test.rs
@@ -1,11 +1,12 @@
-use std::thread::sleep_ms;
+use core::time;
+use std::thread::sleep;
 
 use libwebrtc_sys::system_time_millis;
 
 #[test]
 fn it_works() {
     let a: i32 = system_time_millis().parse().unwrap();
-    sleep_ms(2000);
+    sleep(time::Duration::from_millis(2000));
     let b: i32 = system_time_millis().parse().unwrap();
 
     assert!((a - b) < 3000);
