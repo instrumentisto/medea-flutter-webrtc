cc1e4df6f171fb2f44278fc1104bd8d2f610b130
alexlapa <lapa.alex@ex.ua>
2021-12-20 12:10:15 +0200

    wip

--- crates/native/build.rs
+++ crates/native/build.rs
@@ -4,7 +4,8 @@ use anyhow::{anyhow, Context};
 use walkdir::{DirEntry, WalkDir};
 
 fn main() -> anyhow::Result<()> {
-    cxx_build::bridge("src/lib.rs").compile("jason_flutter_webrtc");
+    let package_name = env::var("CARGO_PKG_NAME")?.replace("-", "_");
+    cxx_build::bridge("src/lib.rs").compile(&package_name);
 
     copy_cxxbridge1_lib()?;
 
