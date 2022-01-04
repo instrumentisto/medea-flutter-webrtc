80182ae17daf24c3a2cb239ffabb7f0eff2e29ec
rogurotus <rogurotus@gmail.com>
2022-01-04 13:56:29 +0300

    add depend

--- windows/src/flutter_webrtc.cc
+++ windows/src/flutter_webrtc.cc
@@ -8,6 +8,30 @@
 
 namespace flutter_webrtc_plugin {
 
+template <typename T>
+inline bool TypeIs(const EncodableValue val) {
+  return std::holds_alternative<T>(val);
+}
+
+template <typename T>
+inline const T GetValue(EncodableValue val) {
+  return std::get<T>(val);
+}
+
+inline EncodableMap findMap(const EncodableMap& map, const std::string& key) {
+  auto it = map.find(EncodableValue(key));
+  if (it != map.end() && TypeIs<EncodableMap>(it->second))
+    return GetValue<EncodableMap>(it->second);
+  return EncodableMap();
+}
+
+inline std::string findString(const EncodableMap& map, const std::string& key) {
+  auto it = map.find(EncodableValue(key));
+  if (it != map.end() && TypeIs<std::string>(it->second))
+    return GetValue<std::string>(it->second);
+  return std::string();
+}
+
 FlutterWebRTC::FlutterWebRTC(FlutterWebRTCPlugin* plugin) {}
 
 FlutterWebRTC::~FlutterWebRTC() {}
