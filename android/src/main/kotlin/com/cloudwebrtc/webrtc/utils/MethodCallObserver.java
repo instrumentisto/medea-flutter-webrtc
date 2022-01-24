e66f3b6318ccbbe8b384cc9a87a09d64299505bc
evdokimovs <evdokimovs@protonmail.ch>
2022-01-24 12:34:52 +0300

    Remove old Java code

--- android/src/main/kotlin/com/cloudwebrtc/webrtc/utils/MethodCallObserver.java
+++
@@ -1,8 +0,0 @@
-package com.cloudwebrtc.webrtc.utils;
-
-import androidx.annotation.NonNull;
-import io.flutter.plugin.common.MethodCall;
-
-public interface MethodCallObserver {
-  void observerMethodCall(MethodCall call, @NonNull AnyThreadResult result);
-}
