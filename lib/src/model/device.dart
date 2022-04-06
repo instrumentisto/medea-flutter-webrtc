576449ba409a591c4759c8982290728fe77c9a3e
logist322 <hytornoy-99@mail.ru>
2022-04-06 16:56:31 +0400

    Fix conditional compilation

--- lib/src/model/device.dart
+++ lib/src/model/device.dart
@@ -1,8 +1,4 @@
-// TODO(alexlapa):
-//    i assume that this won't work on android
-//    and we need none of this for web build
-export '/src/model/ffi/device.dart'
-    if (dart.library.html) '/src/model/channel/device.dart';
+import '/src/api/bridge.g.dart' as ffi;
 
 /// Media device kind.
 enum MediaDeviceKind {
@@ -14,4 +10,32 @@ enum MediaDeviceKind {
 
   /// Represents a video input device (for example, a webcam).
   videoinput,
+}
+
+/// Information about some media device.
+class MediaDeviceInfo {
+  /// Creates a [MediaDeviceInfo] basing on the [Map] received from the native
+  /// side.
+  MediaDeviceInfo.fromMap(dynamic map) {
+    deviceId = map['deviceId'];
+    label = map['label'];
+    kind = MediaDeviceKind.values[map['kind']];
+  }
+
+  /// Creates a [MediaDeviceInfo] basing on the [ffi.MediaDeviceInfo] received
+  /// from the native side.
+  MediaDeviceInfo.fromFFI(ffi.MediaDeviceInfo info) {
+    deviceId = info.deviceId;
+    label = info.label;
+    kind = MediaDeviceKind.values[info.kind.index];
+  }
+
+  /// Identifier of the represented device.
+  late String deviceId;
+
+  /// Human-readable device description (for example, "External USB Webcam").
+  late String label;
+
+  /// Media kind of the device (for example, `audioinput` for microphone).
+  late MediaDeviceKind kind;
 }
