41dbee80cb72b8a0daf573acad009872e5cd6032
alexlapa <lapa.alex@ex.ua>
2022-05-04 18:16:47 +0300

    corrections

--- example/android/app/src/main/kotlin/com/example/flutter_webrtc_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/flutter_webrtc_example/MainActivity.kt
@@ -5,7 +5,4 @@ import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
-    }
 }
