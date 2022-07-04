ef693603b6ffa52e9e0f1861b7d7cca88097638d
evdokimovs <evdokimovs@protonmail.ch>
2022-07-04 14:35:23 +0200

    Fix

--- crates/libwebrtc-sys/src/bridge.rs
+++ crates/libwebrtc-sys/src/bridge.rs
@@ -30,7 +30,8 @@ type DynTrackEventCallback = Box<dyn TrackEventCallback>;
 #[allow(
     clippy::expl_impl_clone_on_copy,
     clippy::items_after_statements,
-    clippy::ptr_as_ptr
+    clippy::ptr_as_ptr,
+    clippy::trait-duplication-in-bounds,
 )]
 #[cxx::bridge(namespace = "bridge")]
 pub(crate) mod webrtc {
