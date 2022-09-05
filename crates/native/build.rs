36daeac09052aa38cfd3951efbf2e0770c2def9e
evdokimovs <evdokimovs@protonmail.ch>
2022-09-05 17:15:46 +0200

    Fix macOS version

--- crates/native/build.rs
+++ crates/native/build.rs
@@ -5,8 +5,9 @@ use std::{env, path::PathBuf, process};
 
 #[allow(clippy::unnecessary_wraps)]
 fn main() -> anyhow::Result<()> {
-    #[cfg(target_os = "macos")]
-    {
+    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
+    if taget_os == "macos" {
+        println!("cargo:rustc-env=MACOSX_DEPLOYMENT_TARGET=10.11");
         println!("cargo:rustc-link-arg=-Wl,-undefined,dynamic_lookup");
         println!(
             "cargo:rustc-link-arg=-Wl,-install_name,\
