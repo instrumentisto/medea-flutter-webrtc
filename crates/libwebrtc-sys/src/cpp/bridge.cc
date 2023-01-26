d971ec4d8956653e268a211d9f8283f535b81d14
evdokimovs <evdokimovs@protonmail.ch>
2023-01-26 13:53:19 +0100

    Fix and video renderer rotation

--- crates/libwebrtc-sys/src/cpp/bridge.cc
+++ crates/libwebrtc-sys/src/cpp/bridge.cc
@@ -454,9 +454,6 @@ std::unique_ptr<VideoSinkInterface> create_forwarding_video_sink(
 void video_frame_to_abgr(const webrtc::VideoFrame& frame, uint8_t* dst_abgr) {
   rtc::scoped_refptr<webrtc::I420BufferInterface> buffer(
       frame.video_frame_buffer()->ToI420());
-  // Width should be aligned to 4 for dst_stride_abgr argument.
-  int aligned_width = (buffer->width() + 3) & ~3;
-
   libyuv::I420ToABGR(buffer->DataY(), buffer->StrideY(), buffer->DataU(),
                      buffer->StrideU(), buffer->DataV(), buffer->StrideV(),
                      dst_abgr, aligned_width * 4, buffer->width(),
@@ -465,14 +462,11 @@ void video_frame_to_abgr(const webrtc::VideoFrame& frame, uint8_t* dst_abgr) {
 
 // Converts the provided `webrtc::VideoFrame` pixels to the ARGB scheme and
 // writes the result to the provided `dst_argb`.
-void video_frame_to_argb(const webrtc::VideoFrame& frame, uint8_t* dst_argb) {
+void video_frame_to_argb(const webrtc::VideoFrame& frame, int argb_stride, uint8_t* dst_argb) {
   auto buffer = frame.video_frame_buffer()->ToI420();
-  // Width should be aligned to 4 for dst_stride_argb argument.
-  int aligned_width = (buffer->width() + 3) & ~3;
-
   libyuv::I420ToARGB(buffer->DataY(), buffer->StrideY(), buffer->DataU(),
                      buffer->StrideU(), buffer->DataV(), buffer->StrideV(),
-                     dst_argb, aligned_width * 4, buffer->width(),
+                     dst_argb, argb_stride, buffer->width(),
                      buffer->height());
 }
 
--- lib/src/platform/web/video_renderer.dart
+++ lib/src/platform/web/video_renderer.dart
@@ -101,6 +101,9 @@ class WebVideoRenderer extends VideoRenderer {
   int get videoHeight => value.height.toInt();
 
   @override
+  int get quarterTurnsRotation => 0;
+
+  @override
   int get textureId => _textureId;
 
   @override
