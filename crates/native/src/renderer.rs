d63ee429c3eedd1923d5ac428f5d7d46d85a788d
rogurotus <rogurotus@gmail.com>
2023-01-30 12:34:55 +0300

    fix clippy

--- crates/native/src/renderer.rs
+++ crates/native/src/renderer.rs
@@ -56,7 +56,7 @@ mod frame_handler {
     #[allow(
         clippy::items_after_statements,
         clippy::trait_duplication_in_bounds,
-        clippy::let_underscore_drop
+        let_underscore_drop,
     )]
     #[cxx::bridge]
     mod cpp_api_bindings {
