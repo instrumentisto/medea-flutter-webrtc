931419da149c200d4db09dc61f31b98b2b0b1822
alexlapa <lapa.alex@ex.ua>
2023-10-02 16:47:01 +0300

    ios

--- ios/Classes/model/Encoding.swift
+++ ios/Classes/model/Encoding.swift
@@ -2,21 +2,24 @@ import WebRTC
 
 /// Encoding describes a single configuration of a codec for an RTCRtpSender.
 class Encoding {
-
-  /// A string which, if set, specifies an RTP stream ID (RID) to be sent using the RID header
+  /// A string which, if set, specifies an RTP stream ID (RID) to be sent using
+  /// the RID header
   /// extension.
-  var rid: String,
+  var rid: String
 
   /// If true, the described encoding is currently actively being used.
-  var active: Boolean,
+  var active: Boolean
 
-  /// Indicates the maximum number of bits per second to allow for this encoding.
+  /// Indicates the maximum number of bits per second to allow for this
+  /// encoding.
   var maxBitrate: Int?
 
-  /// A value specifying the maximum number of frames per second to allow for this encoding.
-  var maxFramerate: Double?,
+  /// A value specifying the maximum number of frames per second to allow for
+  /// this encoding.
+  var maxFramerate: Double?
 
-  /// This is a double-precision floating-point value specifying a factor by which to scale down
+  /// This is a double-precision floating-point value specifying a factor by
+  /// which to scale down
   /// the video during encoding.
   var scaleResolutionDownBy: Double?
 
