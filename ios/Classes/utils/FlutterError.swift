22a18a5eb9093d50a0b4b1e4124f0a3fb5e0cc29
evdokimovs <evdokimovs@protonmail.ch>
2023-12-15 15:08:26 +0100

    Add doc

--- ios/Classes/utils/FlutterError.swift
+++ ios/Classes/utils/FlutterError.swift
@@ -1,3 +1,4 @@
+/// Converts provided Swift `Error` to `FlutterError`.
 func getFlutterError(_ error: Error) -> FlutterError {
     let e = error as NSError
     return FlutterError(code: "Error: \(e.code)", message: e.domain, details: error.localizedDescription)
