c9e853bc9d8da2731d726d20ade89e53567d672d
alexlapa <lapa.alex@ex.ua>
2025-04-16 12:37:58 +0300

    logging [skip ci]

--- crates/libwebrtc-sys/include/video_sink.h
+++ crates/libwebrtc-sys/include/video_sink.h
@@ -4,6 +4,7 @@
 #include <map>
 #include <memory>
 #include <optional>
+#include <chrono>
 
 #include "api/media_stream_interface.h"
 #include "api/video/video_frame.h"
@@ -31,6 +32,8 @@ class ForwardingVideoSink : public rtc::VideoSinkInterface<webrtc::VideoFrame> {
  private:
   // Rust side callback that the `VideoFrame`s will be forwarded to.
   std::optional<rust::Box<bridge::DynOnFrameCallback>> cb_;
+  int n_frames = 0;
+  std::chrono::steady_clock::time_point loged_at = std::chrono::steady_clock::now();
 };
 
 }  // namespace video_sink
--- crates/libwebrtc-sys/src/cpp/video_sink.cc
+++ crates/libwebrtc-sys/src/cpp/video_sink.cc
@@ -1,5 +1,6 @@
 #include "libwebrtc-sys/include/video_sink.h"
 #include "libwebrtc-sys/src/bridge.rs.h"
+#include "rtc_base/logging.h"
 
 namespace video_sink {
 
@@ -10,6 +11,19 @@ ForwardingVideoSink::ForwardingVideoSink(
 
 // Propagates the received `VideoFrame` to the Rust side.
 void ForwardingVideoSink::OnFrame(const webrtc::VideoFrame& video_frame) {
+    ++n_frames;
+
+    auto now = std::chrono::steady_clock::now();
+    auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(
+                          now - loged_at)
+                          .count();
+
+    if (elapsed_ms > 1000) {
+      loged_at = now;
+      RTC_LOG(LS_ERROR) << "ForwardingVideoSink fps: " << n_frames;
+      n_frames = 0;
+    }
+
   bridge::on_frame(*cb_.value(),
                    std::make_unique<webrtc::VideoFrame>(video_frame));
 }
