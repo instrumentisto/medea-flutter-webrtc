b24902983e44063fef958d5fd63d35057958e8ef
tyranron <tyranron@gmail.com>
2025-05-09 13:48:58 +0200

    Minor corrections

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin
+    // Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -22,7 +23,8 @@ android {
     defaultConfig {
         applicationId = "com.instrumentisto.medea_flutter_webrtc_example"
         // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        // For more information, see:
+        // https://flutter.dev/to/review-gradle-config.
         minSdk = 24
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -31,7 +33,8 @@ android {
 
     buildTypes {
         release {
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            // Signing with the debug keys for now, so `flutter run --release`
+            // works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
